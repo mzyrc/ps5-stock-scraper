667f24dd9517eee3da2d3b21163f18c9cf9e8fed
David Manwaring <david.manwaring@assetzcapital.co.uk>
2021-03-12 17:08:58 +0000

    Refactoring and using an incognito browser so I don't store any cookies from each visit

--- src/index.js
+++ src/index.js
@@ -8,14 +8,52 @@ const CHECK_EVERY_NUMBER_OF_SECONDS = 60;
 const CHANNEL_ARN = 'arn:aws:sns:eu-west-2:762197375350:ps5-in-stock';
 const notificationService = new NotificationService(new AWS.SNS({apiVersion: 'latest'}))
 
-main();
+const scrapeListConfig = [
+  {
+    name: 'Game.co.uk',
+    url: 'https://www.game.co.uk/playstation-5',
+    keySectionClassName: '.contentPanelWrapper',
+    checkPage: async (page) => {
+      return page.$$eval('.contentPanelItem', elements => {
+        return elements.filter(element => {
+          const productName = element.querySelector('h3').textContent;
+          const productStatus = element.querySelector('.strapLine').textContent;
+
+          return productStatus && productName === 'PlayStation 5' || productName === 'PlayStation 5 Digital Edition';
+        }).map(element => {
+          return {
+            name: element.querySelector('h3').textContent,
+            status: element.querySelector('.strapLine').textContent,
+            stockStatus: element.querySelector('span').textContent
+          };
+        });
+      });
+    }
+  }
+];
 
 async function main() {
   const browser = await startBrowser();
-  console.log('Scraping...')
-  scrapeAll(browser)
+  const context = await browser.createIncognitoBrowserContext();
+
+  for(const config of scrapeListConfig) {
+    console.log(`Checking ${config.name}`);
+    const productResults = await scrapeAll(context, config);
+
+    productResults.forEach(product => {
+      if (product.stockStatus !== 'Out of stock') {
+        notificationService.publish(`${product.name} is ${product.stockStatus}`, CHANNEL_ARN);
+      } else {
+        console.log(`${product.name} is ${product.stockStatus}`);
+      }
+    })
+  }
+
+  await context.close();
+  await browser.close();
+
   setTimeout(() => {
-    main(browser);
+    main();
   }, CHECK_EVERY_NUMBER_OF_SECONDS * 1000)
 }
 
@@ -31,41 +69,19 @@ async function startBrowser() {
   }
 }
 
-async function scrapeAll(browserInstance) {
+async function scrapeAll(browserInstance, config) {
   try {
     const page = await browserInstance.newPage();
 
     await page.setCacheEnabled(false);
-    await page.goto('https://www.game.co.uk/playstation-5')
-    await page.waitForSelector('.contentPanelWrapper');
+    await page.goto(config.url)
+    await page.waitForSelector(config.keySectionClassName);
 
-    const productResults = await page.$$eval('.contentPanelItem', elements => {
-      return elements.filter(element => {
-        const productName = element.querySelector('h3').textContent;
-        const productStatus = element.querySelector('.strapLine').textContent;
-
-        return productStatus && productName === 'PlayStation 5' || productName === 'PlayStation 5 Digital Edition';
-      }).map(element => {
-        return {
-          name: element.querySelector('h3').textContent,
-          status: element.querySelector('.strapLine').textContent,
-          stockStatus: element.querySelector('span').textContent
-        };
-      });
-    });
-
-    await browserInstance.close();
-
-    productResults.forEach(product => {
-      if (product.stockStatus !== 'Out of stock') {
-        notificationService.publish(`${product.name} is ${product.stockStatus}`, CHANNEL_ARN);
-      } else {
-        console.log(`${product.name} is ${product.stockStatus}`);
-      }
-    })
-
+    return config.checkPage(page);
   } catch (error) {
     console.log(error.name);
     console.log(error);
   }
 }
+
+main();
